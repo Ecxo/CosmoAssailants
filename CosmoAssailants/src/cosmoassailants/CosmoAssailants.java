2a923cf19c32f7a185ea011f689b57b7cee0b618
Petri T Pihlajaniemi <ptpihlaj@dcb-16.cs.helsinki.fi>
2013-09-04 14:48:07 +0300

    Aloitetaan

---
+++ CosmoAssailants/src/cosmoassailants/CosmoAssailants.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cosmoassailants;
+
+/**
+ *
+ * @author ptpihlaj
+ */
+public class CosmoAssailants {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}

f91faf5c6f366e78dcf80d4d9647ef6c20aebbaa
Petri T Pihlajaniemi <ptpihlaj@dat-10.cs.helsinki.fi>
2013-09-18 14:28:45 +0300

    luokka jossa static-arvoja

---
+++ CosmoAssailants/src/cosmoassailants/Gamelogic/gameValues.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cosmoassailants.Gamelogic;
+
+/**
+ *
+ * @author ptpihlaj
+ */
+public class gameValues {
+    public static final int ARENA_SIZE_X = 800;
+    public static final int ARENA_SIZE_Y = 600;
+    
+}
